9ef78c00db530718b14319cad637f7d0ae367502
Ashes <792634614@qq.com>
2021-05-11 17:28:24 +0800

    update ;pg

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
 FROM base_image;
 
-RUN apt-get -qqy install openssh-server
-
 # Using in dev
 ADD ./ /home/admin/app
 WORKDIR /home/admin/app
-RUN npm install
 ENTRYPOINT /usr/sbin/sshd && \
 mkdir tempAsset && \
-npm run build && \
 node bundle/index.js 
 
 EXPOSE 22 80 443
--- src/util/log.ts
+++ src/util/log.ts
@@ -1,113 +1,165 @@
-import { isDev, JS_ERROR_LOG_PATH, JS_LOG_PATH, NETWORK_LOG_PATH, NETWORK_SOURCE_PATH } from './constant';
-import * as fs from 'fs';
+import {
+  BASE_PATH,
+  // JS_ERROR_LOG_PATH, JS_LOG_PATH, NETWORK_LOG_PATH, NETWORK_SOURCE_PATH
+} from './constant';
+// import * as fs from 'fs';
 import * as puppeteer from 'puppeteer';
-import { ConsoleMessage, PageEventObj, Request } from 'puppeteer';
+// import { ConsoleMessage, PageEventObj, Request } from 'puppeteer';
+import * as xlsx from 'xlsx';
 
-type CustomStream = string | fs.WriteStream;
-interface LogSetItem<ITEMTYPE> {
-  jsError: string | fs.WriteStream;
-  jsLog: string | fs.WriteStream;
-  networkLog: string | fs.WriteStream;
+interface XlsxRowData {
+  name?: number | string;
+  url: string;
+  headers: string;
+  startTime?: string;
+  endTime?: string;
+  response: string;
+  spendTime?: number;
+  postData?: string;
+  status: number | string;
 }
 
-interface LogSet<T = string | fs.WriteStream> {
-  [key: string]: LogSetItem<T>;
+// type CustomStream = string | fs.WriteStream;
+interface LogSetItem {
+  http: XlsxRowData[];
+  websocket: XlsxRowData[];
+  resource: XlsxRowData[];
+  // request: XlsxRowData[];
+}
+
+interface LogSet {
+  [key: string]: LogSetItem;
 }
 
 class Logs {
   private logs: LogSet = {};
 
-  setLog = <T>(key: string, option: LogSetItem<T>) => {
+  setLog = (key: string, option: LogSetItem) => {
     this.logs[key] = option;
+    return this.logs[key];
   };
 
   getLogs = () => this.logs;
 
-  getLog = <T>(key: string): LogSetItem<T> => this.logs[key];
+  getLog = (key: string): LogSetItem => this.logs[key];
 }
 
 const logs = new Logs();
 
-const sumContent = (stream: CustomStream, content: string) => {
-  if (typeof stream === 'string') {
-    stream += `\n${content}`;
-  } else {
-    stream.write(content);
-  }
-};
+const httpAnaylze = async (log: LogSetItem, page: puppeteer.Page) => {
+  // page.on('request', (req) => {});
 
-const consoleAnalyze = (chunk: ConsoleMessage, stream: CustomStream) => {
-  sumContent(stream, chunk.text());
-};
-
-const networkAnalyze = async (chunk: Request, stream: CustomStream) => {
-  const res = chunk.response();
-  if (!res) {
-    return;
-  }
-  if (chunk.resourceType() === 'xhr') {
-    try {
-      const json = (await res.json()) || 'no response';
-      sumContent(stream, JSON.stringify(json));
-    } catch (error) {
-      const text = await res.text();
-      console.log(text);
-
-      sumContent(stream, text);
+  page.on('requestfinished', async (req) => {
+    const type = req.resourceType();
+    if (type === 'xhr') {
+      log.http.push({
+        headers: JSON.stringify(req.headers()),
+        url: req.url(),
+        endTime: req.response()?.headers().date || 'unknow',
+        postData: req.postData() || 'no post param',
+        response: (await req.response()?.text()) || 'no response content',
+        status: req.response()?.status() || 'no status',
+      });
+    } else {
+      log.resource.push({
+        headers: JSON.stringify(req.headers()),
+        url: req.url(),
+        endTime: req.response()?.headers().date || 'unknow',
+        postData: req.postData() || 'no post param',
+        response: (await req.response()?.text()) || 'no response content',
+        status: req.response()?.status() || 'no status',
+      });
     }
-  } else {
-    const text = await res.text();
-    sumContent(stream, text);
-  }
-};
-const errorAnalyze = (chunk: Error, stream: CustomStream) => {
-  sumContent(stream, chunk.message);
+  });
 };
 
-const contentAnalyze = (type: keyof PageEventObj, chunk: ConsoleMessage | Request | Error, stream: CustomStream) => {
-  switch (type) {
-    case 'requestfinished' || 'requestfailed':
-      networkAnalyze(chunk as Request, stream);
-      break;
-    case 'console':
-      consoleAnalyze(chunk as ConsoleMessage, stream);
-      break;
-    default:
-      errorAnalyze(chunk as Error, stream);
-  }
+const websocketAnaylze = async (log: LogSetItem, page: puppeteer.Page) => {
+  const cdp = await page.target().createCDPSession();
+  await cdp.send('Network.enable');
+  // await cdp.send('Page.enable');
+
+  cdp.on('Network.webSocketClosed', function (params) {
+    // console.log(`WebSocket 连接关闭`);
+    log.websocket.push({
+      url: params.url,
+      headers: 'websocket',
+      endTime: new Date().toTimeString(),
+      postData: 'close',
+      response: 'close success',
+      status: 200,
+    });
+  });
+  cdp.on('Network.webSocketFrameError', function (params) {
+    // console.log(`WebSocket error`, params);
+    log.websocket.push({
+      url: params.url,
+      headers: 'websocket',
+      endTime: new Date().toTimeString(),
+      postData: 'error',
+      response: 'websocket error',
+      status: 500,
+    });
+  });
+  cdp.on('Network.webSocketCreated', (data) => {
+    console.log('trigger websocket create', data);
+    log.websocket.push({
+      url: data.url,
+      headers: 'websocket',
+      endTime: new Date().toTimeString(),
+      postData: 'create',
+      response: 'websocket connected',
+      status: 200,
+    });
+  });
+  cdp.on('Network.webSocketFrameReceived', (data) => {
+    console.log('trigger websocket', data);
+    log.websocket.push({
+      url: data.url,
+      headers: 'websocket',
+      endTime: new Date().toTimeString(),
+      postData: 'create',
+      response: data.response.payloadData,
+      status: 200,
+    });
+  });
+  // cdp.on('Network.webSocketFrameSent', (data) => console.log(data));
 };
 
 export const startLogRecorder = async (key: string, page: puppeteer.Page) => {
-  const pageerrorWriteStream = fs.createWriteStream(JS_ERROR_LOG_PATH(key), { flags: 'a' });
-  const requestfinishedWriteStream = fs.createWriteStream(NETWORK_LOG_PATH(key), { flags: 'a' });
-  const consoleWriteStream = fs.createWriteStream(JS_LOG_PATH(key), { flags: 'a' });
-
-  const sourceWriteStream = fs.createWriteStream(NETWORK_SOURCE_PATH(key), { flags: 'a' });
-
-  logs.setLog<fs.WriteStream>(key, {
-    jsError: pageerrorWriteStream,
-    jsLog: consoleWriteStream,
-    networkLog: requestfinishedWriteStream,
+  const log = logs.setLog(key, {
+    websocket: [],
+    http: [],
+    resource: [],
   });
-  page.on('requestfinished', (chunk) => contentAnalyze('requestfinished', chunk, requestfinishedWriteStream));
-  page.on('requestfinished', (chunk) => contentAnalyze('requestfinished', chunk, sourceWriteStream));
-  page.on('console', (chunk) => contentAnalyze('console', chunk, requestfinishedWriteStream));
-  page.on('pageerror', (chunk) => contentAnalyze('pageerror', chunk, requestfinishedWriteStream));
+  httpAnaylze(log, page);
+  websocketAnaylze(log, page);
 };
 
 export const stopLogRecorder = async (key: string, page: puppeteer.Page) => {
   const log = logs.getLog(key);
-  const { jsError, jsLog, networkLog } = log;
+  const { http, resource, websocket } = log;
+  // console.log(request);
+  const wb = xlsx.utils.book_new();
+  wb.SheetNames.push('http');
+  wb.SheetNames.push('resource');
+  wb.SheetNames.push('websocket');
+  const httpWS = xlsx.utils.json_to_sheet(http);
+  const resourceWS = xlsx.utils.json_to_sheet(resource);
+  // wb.Sheets['request'];
+  wb.Sheets['http'] = httpWS;
+  wb.Sheets['resource'] = resourceWS;
+  wb.Sheets['websocket'] = websocket;
+  xlsx.writeFile(wb, `${BASE_PATH}${key}/log.xlsx`);
   page.removeAllListeners();
-  if (!(typeof jsError === 'string')) {
-    jsError.close();
-  }
-  if (!(typeof jsLog === 'string')) {
-    jsLog.close();
-  }
-  if (!(typeof networkLog === 'string')) {
-    networkLog.close();
-  }
+  // if (!(typeof jsError === 'string')) {
+  //   jsError.close();
+  // }
+  // if (!(typeof jsLog === 'string')) {
+  //   jsLog.close();
+  // }
+  // if (!(typeof networkLog === 'string')) {
+  //   networkLog.close();
+  // }
 };
 
 export default logs;
